6bba82eee6dfd79ceb0def3fa4356a4414b92623
Thorben Werner <w.thorben94@gmail.com>
2023-08-08 11:22:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("answer is 5")
+from unittest import TestCase
+
+class TryTesting(TestCase):
+    def test_always_passes(self):
+        self.assertTrue(True)
+
+    def test_always_fails(self):
+        self.assertTrue(False)
