db6eb10c3dfdfde1137b5dc537f88cd9e37dbc52
Thorben Werner <w.thorben94@gmail.com>
2023-08-08 11:43:52 +0200

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,1 +1,11 @@
+from unittest import TestCase
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--number", type=str, required=True)
+
+class TryTesting(TestCase):
+    def test_always_passes(self):
+        self.assertTrue(True)
+
+    def test_always_fails(self):
+        self.assertTrue(False)
